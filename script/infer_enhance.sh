bb8046b3ce94c4014b3973a3ced8fc8f6a559ba4
hpnquoc <hpnquoc@skku.edu>
2024-10-22 17:35:40 +0900

    update autoscript

---
+++ script/infer_enhance.sh
@@ -0,0 +1,44 @@
+############################################
+# AUU RainSnow dataset inference
+
+# # Egenvej
+# python infer.py --dir_image "data/AAU RainSnow/Egensevej/Egensevej-1/" --dir_output output/aaurainsnow/Egensevej/Egensevej-1/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Egensevej/Egensevej-2/" --dir_output output/aaurainsnow/Egensevej/Egensevej-2/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Egensevej/Egensevej-3/" --dir_output output/aaurainsnow/Egensevej/Egensevej-3/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Egensevej/Egensevej-4/" --dir_output output/aaurainsnow/Egensevej/Egensevej-4/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Egensevej/Egensevej-5/" --dir_output output/aaurainsnow/Egensevej/Egensevej-5/ --ext png
+# # Hadsundvej
+# python infer.py --dir_image "data/AAU RainSnow/Hadsundvej/Hadsundvej-1/" --dir_output output/aaurainsnow/Hadsundvej/Hadsundvej-1/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Hadsundvej/Hadsundvej-2/" --dir_output output/aaurainsnow/Hadsundvej/Hadsundvej-2/ --ext png
+# # Hasserisvej
+# python infer.py --dir_image "data/AAU RainSnow/Hasserisvej/Hasserisvej-1/" --dir_output output/aaurainsnow/Hasserisvej/Hasserisvej-1/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Hasserisvej/Hasserisvej-2/" --dir_output output/aaurainsnow/Hasserisvej/Hasserisvej-2/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Hasserisvej/Hasserisvej-3/" --dir_output output/aaurainsnow/Hasserisvej/Hasserisvej-3/ --ext png
+# # Hjørringvej
+# python infer.py --dir_image "data/AAU RainSnow/Hjørringvej/Hjørringvej-1/" --dir_output output/aaurainsnow/Hjørringvej/Hjørringvej-1/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Hjørringvej/Hjørringvej-2/" --dir_output output/aaurainsnow/Hjørringvej/Hjørringvej-2/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Hjørringvej/Hjørringvej-3/" --dir_output output/aaurainsnow/Hjørringvej/Hjørringvej-3/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Hjørringvej/Hjørringvej-4/" --dir_output output/aaurainsnow/Hjørringvej/Hjørringvej-4/ --ext png
+# # Hobrovej
+# python infer.py --dir_image "data/AAU RainSnow/Hobrovej/Hobrovej-1/" --dir_output output/aaurainsnow/Hobrovej/Hobrovej-1/ --ext png
+# # Ostre
+# python infer.py --dir_image "data/AAU RainSnow/Ostre/Ostre-1/" --dir_output output/aaurainsnow/Ostre/Ostre-1/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Ostre/Ostre-2/" --dir_output output/aaurainsnow/Ostre/Ostre-2/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Ostre/Ostre-3/" --dir_output output/aaurainsnow/Ostre/Ostre-3/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Ostre/Ostre-4/" --dir_output output/aaurainsnow/Ostre/Ostre-4/ --ext png
+# # Ringvej
+# python infer.py --dir_image "data/AAU RainSnow/Ringvej/Ringvej-1/" --dir_output output/aaurainsnow/Ringvej/Ringvej-1/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Ringvej/Ringvej-2/" --dir_output output/aaurainsnow/Ringvej/Ringvej-2/ --ext png
+# python infer.py --dir_image "data/AAU RainSnow/Ringvej/Ringvej-3/" --dir_output output/aaurainsnow/Ringvej/Ringvej-3/ --ext png
+
+
+############################################
+# AICity Challenge 2021 dataset inference
+
+# python infer.py --dir_image "data/AIC21/Track 1/cam_1/" --dir_output "output/AIC21/Track 1/cam_1/" --ext png
+# python infer.py --dir_image "data/AIC21/Track 1/cam_3/" --dir_output "output/AIC21/Track 1/cam_3/" --ext png
+
+
+############################################
+# vehiclehah dataset inference
+# python infer.py --dir_image "data/vehiclehah/" --dir_output "output/vehiclehah/" --ext jpg
